7d702e27242a68a628ddec8539bd1f291a6f802f
mryash04 <yash.04may@gmail.com>
2020-11-18 14:17:29 +0530

    reactperson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>{
+    return(
+        <React.Fragment>
+            <div className="person-lists">
+                <Personlist />
+            </div>
+        </React.Fragment>
+    )
+};
+
+const Personlist = () =>{
+
+    const people = [
+        {
+            img: 74,
+            name: 'Mark',
+            job: 'Web Developer'
+        },
+        {
+            img: 72,
+            name: 'Tesluien',
+            job: 'Marketing management'
+        },
+        {
+            img: 78,
+            name: 'Paul Boag',
+            job: 'UI/UX Designer'
+        }
+    ];
+
+    return(
+        <React.Fragment>
+            <div className="person-peoplelists">
+                <Person listpeople = {people[0]} />
+                <Person listpeople = {people[1]}
+                        text="Marketing is an expansive area of expertise that 
+                        encompasses many different focus areas,and job descriptions"
+                 />
+                <Person listpeople = {people[2]}  />
+            </div>
+        </React.Fragment>
+    )
+};
+
+const Person = (props) =>{
+
+    const url =`https://randomuser.me/api/portraits/thumb/men/${props.listpeople.img}.jpg`;
+
+    return(
+        <React.Fragment>
+            <div className="person">
+                <div className="person-image">
+                    <img src={url} alt="personimage" />
+                </div>
+                <div className="information">
+                    <h2>{props.listpeople.name}</h2>
+                    <span style={{padding:"5px"}}>{props.text}</span>
+                    <p>{props.listpeople.job}
+                    </p>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+};
 
 export default App;
